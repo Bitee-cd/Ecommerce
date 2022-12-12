523286d586557d8eec5cdeb122f3c3cefc87e655
Bitee-cd <Calebduniya45@gmail.com>
2022-12-12 06:33:41 +0100

    connection to mongodb and seeding

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,7 @@
-import Link from 'next/link';
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import Layout from '../components/Layout';
+import Link from "next/link";
+import React from "react";
+import { useForm } from "react-hook-form";
+import Layout from "../components/Layout";
 
 const Login = () => {
   const {
@@ -9,7 +9,9 @@ const Login = () => {
     register,
     formState: { errors },
   } = useForm();
-  const submitHandler = ({ email, password }) => {};
+  const submitHandler = ({ email, password }) => {
+    console.log(email, password);
+  };
   return (
     <Layout title="Login">
       <form
@@ -20,8 +22,8 @@ const Login = () => {
         <div className="mb-4">
           <label htmlFor="email">Email</label>
           <input
-            {...register('email', {
-              required: 'Please Enter your email',
+            {...register("email", {
+              required: "Please Enter your email",
               pattern: {
                 value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
               },
@@ -38,9 +40,9 @@ const Login = () => {
         <div className="mb-4">
           <label htmlFor="password">Password</label>
           <input
-            {...register('password', {
-              required: 'Please enter your password',
-              minLength: { value: 6, message: 'password is more than 5 chars' },
+            {...register("password", {
+              required: "Please enter your password",
+              minLength: { value: 6, message: "password is more than 5 chars" },
             })}
             type="password"
             className="w-full"
